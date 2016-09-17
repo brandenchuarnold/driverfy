c7df8bb94ebdfe20e6bdb438be1a118016bc50ac
= <brandena@vt.edu>
2016-09-17 16:43:30 -0400

    Added stuff for routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from routes import add_routes
 
 app = Flask(__name__)
+add_routes(app)
 from app import views
---
+++ routes/__init__.py
@@ -0,0 +1,21 @@
+from flask import (
+    Blueprint,
+    current_app as app,
+    jsonify
+)
+
+from routes.playlist import (
+    playlist_route
+)
+from routes.user import (
+    user_route
+)
+from routes.session import (
+    session_route
+)
+
+def add_routes(app):
+    pass
+    app.register_blueprint(playlist_route, url_prefix="/playlist")
+    app.register_blueprint(login_route, url_prefix="/login")
+    app.register_blueprint(session_route, url_prefix="/session")
